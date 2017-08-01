ed151dae76dcac5e5407ebc8ea670994573384e7
QKKQQK <zhouqiankang@hotmail.com>
2017-08-01 11:18:07 -0500

    Configure segue

--- SecondTableView/NumbersTableTableViewController.swift
+++ SecondTableView/NumbersTableTableViewController.swift
@@ -87,14 +87,17 @@ class NumbersTableTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showRow" {
+            let controller = segue.destination as! RowViewController
+            let selectedCell = sender as! UITableViewCell
+            controller.rowText = selectedCell.textLabel?.text
+        }
     }
-    */
+    
 
 }
---
+++ SecondTableView/RowViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RowViewController.swift
+//  SecondTableView
+//
+//  Created by Qiankang Zhou on 8/1/17.
+//  Copyright © 2017 Qiankang Zhou. All rights reserved.
+//
+
+import UIKit
+
+class RowViewController: UIViewController {
+
+    @IBOutlet weak var rowDisplay: UILabel!
+    
+    var rowText: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rowDisplay.text = rowText
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

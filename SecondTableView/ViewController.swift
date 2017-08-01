25ee3277df3c2b947804e13ebbe06ade73f6cbb3
QKKQQK <zhouqiankang@hotmail.com>
2017-08-01 10:19:57 -0500

    Initial Commit

---
+++ SecondTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondTableView
+//
+//  Created by Qiankang Zhou on 8/1/17.
+//  Copyright © 2017 Qiankang Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
